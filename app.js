7d676681bfa03c3cb3841773e16cd00d47cd5210
minseong-jo <minseong4375@gmail.com>
2022-08-11 06:32:26 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+const home = require("./routes/home");
+app.use("/", home);
+
+module.exports = app;
---
+++ bin/www.js
@@ -0,0 +1,6 @@
+const app = require("../app");
+const PORT = 4040;
+
+app.listen(PORT, () => {
+  console.log('This server is running!');
+});
