0063953843fc9c3c122800ff28d0834ce1d6a0e2
minseong-jo <minseong4375@gmail.com>
2022-08-11 22:58:22 +0900

    모듈화 및 최적화

--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -1,4 +1,4 @@
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 const output = {
   index: (req, res) => {
     res.render("home/index");
@@ -11,24 +11,9 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id;
-    const pw = req.body.pw;
 
-    const users= UserStorage.getUsers("id","pw");
-    
-    const response = {};
-    if (users.id.includes(id)) {
-
-      const idx = users.id.indexOf(id);
-      if (users.pw[idx] === pw) {
-        
-        response.success = true;
-        return res.json(response);
-      }
-    }
-
-    response.success = false;
-    response.msg = "로그인에 실패함!";
+    const user = new User(req.body);
+    const response = user.login();
     return res.json(response);
   },
 }
